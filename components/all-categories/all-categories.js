8f17c5d7fbc5b78d7618b2e3147bd6f48a556668
matthias-blumenschein <matthiasblumenschein@gmail.com>
2023-01-24 16:14:06 +0100

    styling: add styling for categories buttons depending on thumbnail state

--- components/all-categories/all-categories.js
+++ components/all-categories/all-categories.js
@@ -5,23 +5,32 @@ import { uid } from "uid";
 import styles from "./all-categories.module.css";
 
 const AllCategories = ({ allCategories, showThumbnail }) => {
+	// je nach status von showThumbnail die Klasse für Link setzen
+
 	return (
 		<div key={uid()} className={styles.allCategories}>
-			<h1>Categories</h1>
-			{allCategories.map((element) => (
-				<Link key={uid()} href={`/category/${element.strCategory}`}>
-					{showThumbnail && (
-						<Image
-							src={element.strCategoryThumb}
-							alt={element.strCategory}
-							width={50}
-							height={50}
-							priority
-						/>
-					)}
-					<p>{element.strCategory}</p>
-				</Link>
-			))}
+			<h3>Categories</h3>
+			<div className={showThumbnail ? styles.catList : styles.catListWithoutTN}>
+				{allCategories.map((element) => (
+					<Link
+						key={uid()}
+						href={`/category/${element.strCategory}`}
+						className={showThumbnail ? styles.catItem : styles.catItemWithoutTN}
+					>
+						{showThumbnail && (
+							<Image
+								src={element.strCategoryThumb}
+								alt={element.strCategory}
+								width={50}
+								height={50}
+								priority
+								className={styles.catImg}
+							/>
+						)}
+						<p>{element.strCategory}</p>
+					</Link>
+				))}
+			</div>
 		</div>
 	);
 };
