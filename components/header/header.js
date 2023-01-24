3766bfdc3f3493b06af2139c7ad5a72d7e20bc44
Elmin Musija <office.elmin.musija@gmail.com>
2023-01-24 20:29:10 +0100

    fix: hide login button on login page

--- components/header/header.js
+++ components/header/header.js
@@ -3,16 +3,20 @@ import LoginButton from "../login-button/login-button";
 import LogoutButton from "../logout-button/logout-button";
 import { useSession } from "next-auth/react";
 import styles from "./header.module.css";
+import { useRouter } from "next/router";
 
 function Header() {
 	const { data: session, status } = useSession();
+	const router = useRouter();
 
 	return (
 		<div className={styles.header}>
 			{session && status === "authenticated" && (
 				<p>You're logged in as {session.user.name || session.user.email}</p>
 			)}
-			{status !== "authenticated" && <LoginButton />}
+			{router.pathname !== "/auth/login" && status !== "authenticated" && (
+				<LoginButton />
+			)}
 			{status === "authenticated" && <LogoutButton />}
 		</div>
 	);
