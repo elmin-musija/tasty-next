fa716bf7a9fdadcdaa846c1fcfbd81bdcf2d6dfd
matthias-blumenschein <matthiasblumenschein@gmail.com>
2023-01-24 14:12:30 +0100

    styling: add styling for areas preview on home page

--- components/preview-areas/preview-areas.js
+++ components/preview-areas/preview-areas.js
@@ -1,19 +1,24 @@
 import React from "react";
 import Link from "next/link";
 import { uid } from "uid";
+import styles from "./preview-areas.module.css";
 
 const PreviewAreas = ({ previewAreas }) => {
 	return (
-		<div key={uid()}>
-			<h2>Areas</h2>
-			<div>
+		<div key={uid()} className="areas-container">
+			<div className={styles.areasHeading}>
+				<h2>Areas</h2>
+				<Link href="/area" className="seeAllLink">
+					See all ...
+				</Link>
+			</div>
+			<div className={styles.areasItem}>
 				{previewAreas.map((element) => (
 					<Link key={uid()} href={`area/${element.strArea}`}>
 						{element.strArea}
 					</Link>
 				))}
 			</div>
-			<Link href="/area">See all ...</Link>
 		</div>
 	);
 };
