642d83d286770556b9660456ab7ce156194edda5
matthias-blumenschein <matthiasblumenschein@gmail.com>
2023-01-24 13:50:50 +0100

    styling: set style of meal of the day card

--- components/meal-of-the-day/meal-of-the-day.js
+++ components/meal-of-the-day/meal-of-the-day.js
@@ -1,23 +1,35 @@
 import React from "react";
 import Link from "next/link";
-import Image from "next/image";
+// import Image from "next/image";
 import { uid } from "uid";
+import styles from "./meal-of-the-day.module.css";
 
 const MealOfTheDay = ({ randomMeal }) => {
 	return (
-		<div key={uid()}>
-			<Link href={`/details/${randomMeal.idMeal}`}>
-				<h2>Meal of the Day</h2>
-				<Image
-					src={randomMeal.strMealThumb}
-					alt={randomMeal.strMeal}
-					width={50}
-					height={50}
-				/>
-				<p key={uid()}>{randomMeal.strMeal}</p>
-				<p key={uid()}>{randomMeal.strCategory}</p>
-				<p key={uid()}>{randomMeal.strArea}</p>
-			</Link>
+		<div>
+			<h2>Meal of the Day</h2>
+			<div key={uid()} className={styles.mealOfDayItem}>
+				<Link href={`/details/${randomMeal.idMeal}`}>
+					{/* <Image
+						src={randomMeal.strMealThumb}
+						alt={randomMeal.strMeal}
+						width={50}
+						height={50}
+					/> */}
+					<div>
+						<p key={uid()} className={styles.mealHeading}>
+							{randomMeal.strMeal}
+						</p>
+						<div className={styles.mealDetailsContainer}>
+							<div className={styles.categoryContainer}>
+								<div className={styles.circle}></div>
+								<p key={uid()}>{randomMeal.strCategory}</p>
+							</div>
+							<p key={uid()}>{randomMeal.strArea}</p>
+						</div>
+					</div>
+				</Link>
+			</div>
 		</div>
 	);
 };
