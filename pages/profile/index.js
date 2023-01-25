e8e22d300b6c48e6f10eb51f87c1f8f25f6260e7
matthias-blumenschein <matthiasblumenschein@gmail.com>
2023-01-25 23:52:00 +0100

    styling: add styling of profile page

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -3,24 +3,35 @@ import { unstable_getServerSession } from "next-auth";
 import { NextAuthOptions } from "../api/auth/[...nextauth]";
 import Image from "next/image";
 import { uid } from "uid";
+import styles from "./profile.module.css";
 
 const ProfilePage = ({ name, email, image }) => {
 	return (
-		<div>
-			<h1>Your Profile</h1>
-			{image && <Image src={image} width={150} height={150} alt={name} />}
-			<p key={uid()}>{name}</p>
-			<h2 key={uid()}>Your Interests</h2>
-			<h2 key={uid()}>Your Account</h2>
-			<p key={uid()}>{email}</p>
-			<h2 key={uid()}>Upgrade to Premium</h2>
-			<p key={uid()}>
-				You have a free Account, upgrade now to Premium for an even better
-				experience with Tasty! The AI algorithm displays you what you've been
-				looking for all day. Save unlimited favourites and interact with the
-				creators of the recipes.
-			</p>
-			<button>Upgrade</button>
+		<div className={styles.profile}>
+			<div>
+				{image && <Image src={image} width={150} height={150} alt={name} />}
+				<h3 key={uid()}>{name}</h3>
+			</div>
+			<div>
+				<h2 key={uid()}>Your Interests</h2>
+			</div>
+			<div>
+				<h2 key={uid()}>Your Account</h2>
+				<p key={uid()}>{email}</p>
+			</div>
+			<div>
+				<h2 key={uid()}>Upgrade to Premium</h2>
+				<p key={uid()} className={styles.premiumText}>
+					You have a free Account, upgrade now to Premium for an even better
+					experience with Tasty! The AI algorithm displays you what you've been
+					looking for all day. Save unlimited favourites and interact with the
+					creators of the recipes.
+				</p>
+				<button className={styles.upgradeBtn}>
+					<p>Upgrade</p>
+					<img src="/arrow-right-white.svg" alt="arrow" />
+				</button>
+			</div>
 		</div>
 	);
 };
