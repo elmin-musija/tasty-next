541143c150e976efc0f614e047b627e9abc5562e
matthias-blumenschein <matthiasblumenschein@gmail.com>
2023-01-25 22:06:38 +0100

    styling: add styling of onboarding page

--- pages/onboarding/index.js
+++ pages/onboarding/index.js
@@ -1,12 +1,20 @@
 import Link from "next/link";
 import React from "react";
+import styles from "./onboarding.module.css";
 
 const OnboardingPage = () => {
 	return (
-		<div>
-			<h1>All recipe you needed</h1>
-			<h2>5000+ healthy recipes made by people for your healthy life</h2>
-			<Link href="/home">Get Started</Link>
+		<div className={styles.onboarding}>
+			<div className={styles.onboardingTop}>
+				<img src="/onboarding.svg" alt="Meals sketches" />
+			</div>
+			<div className={styles.onboardingBottom}>
+				<div>
+					<p>All recipe you needed</p>
+					<p>5000+ healthy recipes made by people for your healthy life</p>
+				</div>
+				<Link href="/home">Get Started</Link>
+			</div>
 		</div>
 	);
 };
