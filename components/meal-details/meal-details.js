6605639e2f32f8ecb751f1d978f520296ddcd1d2
Elmin Musija <office.elmin.musija@gmail.com>
2023-01-25 20:03:24 +0100

    feat: change ingredients and measures to components. ingredients and measures are shown side by side.

--- components/meal-details/meal-details.js
+++ components/meal-details/meal-details.js
@@ -3,16 +3,15 @@ import Image from "next/image";
 import { uid } from "uid";
 import { useSession } from "next-auth/react";
 import FavoritesButton from "../../components/favorites-button/favorites-button";
-import { getIngredients, getMeasures } from "../../utils/data";
+import IngredientMeasure from "../ingredient-measure-table/ingredient-measure-table";
 
 const MealDetails = (props) => {
 	const { data: session, status } = useSession();
 	const { meal } = props;
-	const ingredients = getIngredients(meal);
-	const measures = getMeasures(meal);
+
 	return (
 		<div key={uid()}>
-			<h1>{meal.strMeal}</h1>
+			<h2>{meal.strMeal}</h2>
 			<Image
 				src={meal.strMealThumb}
 				alt={meal.strMeal}
@@ -20,22 +19,19 @@ const MealDetails = (props) => {
 				height={250}
 				priority
 			/>
-			<p key={uid()}>{meal?.strCategory}</p>
-			<p key={uid()}>{meal?.strArea}</p>
+			<h3 key={uid()}>{meal?.strCategory}</h3>
+			<h3 key={uid()}>{meal?.strArea}</h3>
 			<p key={uid()}>{meal?.strInstructions}</p>
-			<p key={uid()}>{meal?.strTags}</p>
+			<h3 key={uid()}>{meal?.strTags}</h3>
 			<a href={meal?.strYoutube} target="_blank">
 				Link to Youtube
 			</a>
 			{session && status === "authenticated" && <FavoritesButton meal={meal} />}
-			<p key={uid()}>Ingredients</p>
-			{ingredients.map((element) => (
-				<p key={uid()}>{element}</p>
-			))}
-			<p key={uid()}>Measures</p>
-			{measures.map((element) => (
-				<p key={uid()}>{element}</p>
-			))}
+
+			<div>
+				<h3 key={uid()}>Ingredients - Measures</h3>
+				<IngredientMeasure meal={meal} />
+			</div>
 		</div>
 	);
 };
