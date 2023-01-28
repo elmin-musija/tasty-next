4073bc8db24c40a6709f51bd1bd7fc1d2faaa981
Elmin Musija <office.elmin.musija@gmail.com>
2023-01-28 15:10:16 +0100

    styling: add typewriter to onboarding page

--- pages/onboarding/index.js
+++ pages/onboarding/index.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import React from "react";
 import styles from "./onboarding.module.css";
 import { motion } from "framer-motion";
+import { Typewriter } from "react-simple-typewriter";
 
 const OnboardingPage = () => {
 	return (
@@ -17,7 +18,26 @@ const OnboardingPage = () => {
 			<div className={styles.onboardingBottom}>
 				<div>
 					<p>All recipe you needed</p>
-					<p>5000+ healthy recipes made by people for your healthy life</p>
+					<p>
+						5000+ tasty recipes made by people for your{" "}
+						<span>
+							<Typewriter
+								words={[
+									"tasty",
+									"healthy",
+									"fresh",
+									"delicious",
+									"sweet",
+									"flavourful",
+								]}
+								loop={2}
+								typeSpeed={100}
+								deleteSpeed={80}
+								delaySpeed={2000}
+							/>
+						</span>{" "}
+						life
+					</p>
 				</div>
 				<Link href="/home">Get Started</Link>
 			</div>
