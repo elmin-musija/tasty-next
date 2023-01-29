5744a70ca646c12848e7d6f9a379272e03474559
matthias-blumenschein <matthiasblumenschein@gmail.com>
2023-01-29 12:33:00 +0100

    styling: introduce equal padding in header, fix margin of searchbar, add larger padding to login and sing in page

--- components/ingredient-measure-table/ingredient-measure-table.js
+++ components/ingredient-measure-table/ingredient-measure-table.js
@@ -1,12 +1,12 @@
 import React from "react";
 import IngredientMeasureItem from "../ingredient-measure-item/ingredient-measure-item";
 import { getIngredientsMeasures } from "../../utils/data";
-import styles from "./ingredient-measure-table.module.css";
+import { uid } from "uid";
 
 const IngredientMeasureTable = ({ meal }) => {
 	const ingredientsMeasures = getIngredientsMeasures(meal);
 	return ingredientsMeasures?.map((element) => (
-		<IngredientMeasureItem item={element} />
+		<IngredientMeasureItem item={element} key={uid()} />
 	));
 };
 
