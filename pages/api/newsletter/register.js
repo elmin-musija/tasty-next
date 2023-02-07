d8085cb0a4ae4b6a8ac41b149d1e5331c09225fb
Elmin Musija <office.elmin.musija@gmail.com>
2023-02-07 22:14:52 +0100

    feat: disable newsletter registration due to privacy policy

--- pages/api/newsletter/register.js
+++ pages/api/newsletter/register.js
@@ -2,28 +2,36 @@ import { sendMail } from "../../../utils/mail";
 
 export default async function handler(req, res) {
 	if (req.method === "POST") {
-		const { email } = JSON.parse(req.body);
 		/**
-		 * TODO: check email...
+		 * Due to privacy policy currently disabled
 		 */
-		const emailOptions = {
-			to: email,
-			subject: "Welcome to the tasty club",
-			message: `Welcome to the tasty club and enjoy our weekly specials!!!`,
-			html: "",
-		};
+		// const { email } = JSON.parse(req.body);
+		// /**
+		//  * TODO: check email...
+		//  */
+		// const emailOptions = {
+		// 	to: email,
+		// 	subject: "Welcome to the tasty club",
+		// 	message: `Welcome to the tasty club and enjoy our weekly specials!!!`,
+		// 	html: "",
+		// };
 
-		const result = sendMail(emailOptions);
-		if (result) {
-			res
-				.status(200)
-				.json({ success: "successfully registered for newsletter." });
-			return;
-		} else {
-			/**
-			 * TODO: handle error...
-			 */
-		}
+		// const result = sendMail(emailOptions);
+		// if (result) {
+		// 	res
+		// 		.status(200)
+		// 		.json({ success: "successfully registered for newsletter." });
+		// 	return;
+		// } else {
+		// 	/**
+		// 	 * TODO: handle error...
+		// 	 */
+		// }
+
+		res
+			.status(500)
+			.json({ warning: "Due to privacy policy currently disabled." });
+		return;
 	}
-	res.status(400).json({ failed: "Invalid request" });
+	res.status(400).json({ error: "Invalid request" });
 }
